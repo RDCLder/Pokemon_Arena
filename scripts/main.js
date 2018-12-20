c94cc4adb6b86014f2e0360376acb973dbb9bfe6
Rigo-Hernandez <hernandez_rigoberto@outlook.com>
2018-12-20 15:16:21 -0600

    rigo

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+tippy('#skill1', {
+    content: "The turn Wish is used, the user will make a wish. At the end of the next turn, the Pokémon in the user's current position will be healed by half its own maximum HP",
+    delay: 100,
+    arrow: true,
+    arrowType: 'round',
+    size: 'large',
+    duration: 500,
+    animation: 'scale',
+    
+})
