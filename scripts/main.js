81b6d6ebb1d14e1ff7b2eb46a2c046c9177159ef
Phong Mach <phongmach@Phongs-MBP.localdomain>
2018-12-21 10:24:12 -0600

    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,108 +1,3 @@
-// let generationName = document.getElementById("generationName")
-// function theItems(result) {
-//     var xhr = new XMLHttpRequest();
-//     xhr.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//         let theObject = JSON.parse(this.responseText);
-//         for (let i = 0; i < theObject.length; i++) {
-//             let theDiv = document.createElement("div");
-//             let generation = document.createTextNode(theObject[i].name);                  
-//             console.log(theObject[i].name)
-//             theDiv.appendChild(generation);
-//             generationName.appendChild(theDiv);
-//             }
-//        }
-//     };
-//     xhr.open("GET", "https://pokeapi.co/docs/api/v2/generation/?limit=" + result + "20", true);
-//     xhr.send(data);
-// };
-// for (let i = 1; i <= 10; i++) {
-//     theItems(i)
-// }
-// var data = null;
-
-// var xhr = new XMLHttpRequest();
-// xhr.withCredentials = true;
-
-// xhr.addEventListener("readystatechange", function () {
-//     if (this.readyState === this.DONE) {
-//         console.log(JSON.parse(this.responseText));
-//     }
-// });
-
-// xhr.open("GET", "https://pokeapi.co/docs/api/v2/generation/");
-
-// xhr.send(data);
-
-// Main file to run
-
-// MVP:  Battle Loop
-
-// import definitions.js
-
-// $(function() {
-
-//     var pokedex = [];
-
-//     $.get("https://pokeapi.co/api/v2/pokemon/")
-//     .done((result) => {
-//         for (let i = 0; i < 1; i ++) {
-//             let pokemon = result.results[i].name;
-//             // pokedex.push([pokemon,]);
-//             // let pokeStats = $.ajax(result.results[i].url).done((result2) => {return result2;});
-//             let pokeStats = $.ajax("https://pokeapi.co/api/v2/pokemon/1/").done((result2) => {return result2;});
-//             pokedex.push([pokemon, pokeStats]);
-//             console.log(pokeStats.abilities);
-//             console.log(pokedex);
-//         }
-//     });
-
-//     $.get("https://pokeapi.co/api/v2/pokemon/1/")
-//     .done((result) => {
-//         console.log(result.abilities);
-//     })
-
-    // console.log(pokedex[3][1].abilities[1].name);
-
-    // var battleStart = true;
-
-    // while (battleStart == true) {
-
-    //     var pokemon1 = [];
-    //     var pokemon2 = [];
-
-    //     // Move 1
-    //     $("#move1").click(() => {
-
-    //         pokemon1.attack(pokemon2, pokemon1.move1); // Maybe store all moves in arr and use random generator to choose specific move
-    //         pokemon2.attack(pokemon1, pokemon1.move1); // Randomize maybe?
-
-    //     });
-
-    //     // Move 2
-    //     $("#move2").click(() => {
-
-    //         pokemon1.attack(pokemon2, pokemon1.move2);
-
-    //     });
-
-    //     // Move 3
-    //     $("#move3").click(() => {
-
-    //         pokemon1.attack(pokemon2, pokemon1.move3);
-
-    //     });
-
-    //     // Move 4
-    //     $("#move4").click(() => {
-
-    //         pokemon1.attack(pokemon2, pokemon1.move4);
-
-    //     });
-
-    // }
-
-// });
 
 // Pokemon priorities
 
@@ -116,16 +11,63 @@
 // Moves are in array from 1-165 in https://pokeapi.co/api/v2/move
 // Power: 
 // PP: 
-// move-ailments:  ["unknown","none","paralysis","sleep","freeze","burn","poison","confusion","trap","disable","leech-seed",]
-// move-category: ["damage", "net-good-stats", "heal", "damage+ailment", "damage+lower", "damage+raise", "damage+heal", "force-switch","unique"]
-// move-damage-classes: 
-// move-learn-method: ["level-up", "machine"]
-// move-target: 
+// move-ailments = ["unknown","none","paralysis","sleep","freeze","burn","poison","confusion","trap","disable","leech-seed",]
+// move-category = ["damage", "net-good-stats", "heal", "damage+ailment", "damage+lower", "damage+raise", "damage+heal", "force-switch","unique"]
+// move-damage-classes = ["status","physical","special"]
+// move-learn-method = ["level-up", "machine"]
+// move-target = ["specific-move","user-field","user","random-opponent","all-other-pokemon","selected-pokemon","all-opponents","entire-field",]
+// Accuracy =
 
-// class Move {
-//     constructor (){
-//         this.Power 
-    
-//     }
+class Move {
+    constructor (moveName, moveObject){
+        this.move = moveName;
+        this.movePower = moveObject.power;
+        this.moveAilments = moveObject.meta.ailment.name;
+        this.moveCategory = moveObject.meta.category.name;
+        this.movePP = moveObject.pp;
+        this.moveDamageClasses = moveObject.damage_class.name;
+        this.moveTarget = moveObject.target.name;
+        this.type = moveObject.type.name
+    }
+     moveStats () {
+        for (let i = 0; i < moveObject.length; i++){
+            let m
+        }
+    }
+}
 
-// }
+// Moves priorities
+// Moves are in array from 1-165 in https://pokeapi.co/api/v2/move
+// Power: 
+// PP: 
+// Priority: 
+// move-ailments = ["unknown","none","paralysis","sleep","freeze","burn","poison","confusion","trap","disable","leech-seed",]
+// move-category = ["damage", "net-good-stats", "heal", "damage+ailment", "damage+lower", "damage+raise", "damage+heal", "force-switch","unique"]
+// move-damage-classes = ["status","physical","special"]
+// move-learn-method = ["level-up", "machine"]
+// move-target = ["specific-move","user-field","user","random-opponent","all-other-pokemon","selected-pokemon","all-opponents","entire-field",]
+// types =
+
+$(function () {
+    var pokeMove = []
+    $.get("https://pokeapi.co/api/v2/move")
+    .done((result) => { 
+        for(var i = 0; i < 165; i++){
+            let moveName = result.results[i].name;
+            
+            $.get(result.results[i].url)
+            .done((result2)=>{
+                pokeMove.push(["Name: " + moveName, "Power: " + result2.power, "PowerPoint: " + result2.pp, "Type: " + result2.type.name])
+            })
+        }
+
+    })
+    console.log(pokeMove)
+
+})
+
+// $(function (){
+//     var pokeStat = []
+//     $.get("https://pokeapi.co/api/v2/move")
+//     .done((result))
+// })
