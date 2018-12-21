444cf861910419b45029f8b962a1ab5242a68cfb
RDCLder <riy2@case.edu>
2018-12-20 21:26:08 -0600

    updated ray

--- scripts/main.js
+++ scripts/main.js
@@ -1,54 +1,272 @@
 // Main file to run
 
-// MVP:  Battle Loop
-
-// import definitions.js
-
 $(function() {
 
-    var pokedex = [];
+    let pokedex = [];
+    let moves = [[], []];
 
+    // Retrieve data from API
+    $.get("https://pokeapi.co/api/v2/move/")
+    .done((result) => {
+        for (let i = 0; i < 165; i ++) {
+            let moveName = result.results[i].name;
+            moves[0].push(moveName);
+
+            // Retrieve object from nested URL
+            $.get(result.results[i].url).done((result) => {
+                moves[1].push(result);
+            });
+        }
+    })
+
+    // Retrieve data from API
     $.get("https://pokeapi.co/api/v2/pokemon/")
     .done((result) => {
         for (let i = 0; i < 151; i ++) {
-            let pokemon = result.results[i].name;
-            pokedex.push([pokemon]);
+            let pokeName = result.results[i].name;
+            pokedex.push([pokeName]);
 
+            // Retrieve object from nested URL
             $.get(result.results[i].url).done((result) => {
-                let pokeStats = result;
-                pokedex[i].push(pokeStats);
+                pokedex[i].push(result);
             });
         }
     });
-        
+    
+    // Account for async with 3000 ms delay
     setTimeout(() => {
-
-        // Things we care about
         
-        // Moves / Example / Template 
-        // Returns a string
-        // console.log(pokedex[1][1].moves[1].move.name);
-        // pokedex[i][1].moves[j].move.name
+        // ------------------------------------------------------------------------------------
 
-        // Sprites / Example / Template
-        // Returns a string URL for a PNG
-        // console.log(pokedex[1][1].sprites.back_default);
-        // pokedex[i][1].sprites.spriteName
         
-        // Stat Name / Stat Value / Example / Template
-        // Returns a string & number
-        // console.log(pokedex[150][1].stats[1].stat.name)
-        // console.log(pokedex[150][1].stats[1].base_stat)
-        // pokedex[i][1].stats[j].stat.name
-        // pokedex[i][1].stats[j].base_stat
+        // Class Definitions
+        
+        class Move {
 
-        // Types / Example / Template
-        // Returns a string
-        console.log(pokedex[150][1].types[0].type.name)
-        // pokedex[i][1].types[j].type.name
+            constructor(moveName, moveObject) {
+                this.name = moveName;
+                this.power = moveObject.power;
+                this.pp = moveObject.pp;
+                this.damageClass = moveObject.damage_class.name;
+                this.type = moveObject.type.name;
+            }
 
-    }, 2000)
+        }
+
+        let pokemon = new Pokemon("Bulbasaur", Object)
+
+        class Pokemon {
+
+            constructor(pokeName, pokeObject) {
+                this.name = pokeName;
+                this.hp = pokeObject.stats[5].base_stat;
+                this.attack = pokeObject.stats[4].base_stat;
+                this.defense = pokeObject.stats[3].base_stat;
+                this.specialAttack = pokeObject.stats[2].base_stat;
+                this.specialDefense = pokeObject.stats[1].base_stat;
+                this.speed = pokeObject.stats[0].base_stat;
+                this.front = pokeObject.sprites.front_default;
+                this.back = pokeObject.sprites.back_default;
+
+                this.moves = [];
+                for (let i = 0; i < 4; i ++) {
+                    let index = Math.floor(Math.random() * pokeObject.moves.length);
+                    let move = pokeObject.moves[index].move.name;
+                    this.moves.push(move);
+                }
+
+                this.type = [];
+                for (let i = 0; i < pokeObject.types.length; i ++) {
+                    let typeName = pokeObject.types[i].type.name;
+                    this.type.push(typeName);
+                }
+            }
+
+            attack(target, move) {
+                // Assuming all pokemon are level 10, no special attacks, and no modifiers
+                let baseDamage = (6 * allMoves[move].power * this.attack / target.defense / 50) + 2;
+                function damageCalc (baseDamage) {
+                    min = Math.ceil(85);
+                    max = Math.floor(100);
+                    return Math.floor(baseDamage * (Math.floor(Math.random() * (max - min + 1)) + min) / 100);
+                }
+                let damage = damageCalc(baseDamage);
+                target.hp -= damage;
+
+                // Message to add to the side display
+                return `${this.name} dealt ${damage} damage to ${target.name}!`
+            }
+
+            status() {
+                return `${this.name} has ${this.hp} health.`
+            }
+
+            alive() {
+                if (this.hp > 0) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+
+        }
+
+        // class Battle necessary?
+
+        // class Battle {
+
+        //     constructor(pokemon1, pokemon2) {
+        //         this.pokemon1 = pokemon1;
+        //         this.pokemon2 = pokemon2;
+        //     }
+
+        //     turn() {
+                
+        //     }
+
+        // }
+
+        // ------------------------------------------------------------------------------------
+
+        // Create Global Variables
+
+        var allPokemon = [];
+        var allMoves = {};
+        
+        for (let i = 0; i < pokedex.length; i ++) {
+            let pokemon = new Pokemon(pokedex[i][0], pokedex[i][1]);
+            allPokemon.push(pokemon);
+        }
+
+        for (let i = 0; i < moves[0].length; i ++) {
+            let move = new Move(moves[0][i], moves[1][i]);
+            let name = moves[0][i];
+            allMoves[name] = move;
+        }
+        // ------------------------------------------------------------------------------------
+
+        // Function Definitions
+
+        function disableButtons() {
+            $("#move1").prop("disabled", true);
+            $("#move2").prop("disabled", true);
+            $("#move3").prop("disabled", true);
+            $("#move4").prop("disabled", true);
+        }
+
+        function enableButtons() {
+            $("#move1").prop("disabled", false);
+            $("#move2").prop("disabled", false);
+            $("#move3").prop("disabled", false);
+            $("#move4").prop("disabled", false);
+        }
+
+        // ------------------------------------------------------------------------------------
+
+        // Main Gameplay
+
+        let turn = 1;
+        let i = Math.floor(Math.random() * allPokemon.length);
+        let j = Math.floor(Math.random() * allPokemon.length);
+        let pokemon1 = allPokemon[i];
+        let pokemon2 = allPokemon[j];
+        let move1 = allMoves[pokemon1.moves[0]];
+        let move2 = allMoves[pokemon1.moves[1]];
+        let move3 = allMoves[pokemon1.moves[2]];
+        let move4 = allMoves[pokemon1.moves[3]];
+
+        $("#move1").click(() => {
+            
+            disableButtons();
+            let i = Math.floor(Math.random() * 4);
+            let enemyMove = allMoves[pokemon2.moves[i]];
+            
+            if (pokemon1.speed < pokemon2.speed) {
+                pokemon2.attack(pokemon1, enemyMove);
+                if (pokemon1.alive() && pokemon2.alive()) {
+                    pokemon1.attack(pokemon2, move1);
+                }
+            }
+            else {
+                pokemon1.attack(pokemon2, move1)
+                if (pokemon1.alive() && pokemon2.alive()) {
+                    pokemon2.attack(pokemon1, enemyMove);
+                }
+            }
+            turn ++;
+            enableButtons();
+        });
 
     
+        $("#move2").click(() => {
+            
+            disableButtons();
+            let i = Math.floor(Math.random() * 4);
+            let enemyMove = allMoves[pokemon2.moves[i]];
+            
+            if (pokemon1.speed < pokemon2.speed) {
+                pokemon2.attack(pokemon1, enemyMove);
+                if (pokemon1.alive() && pokemon2.alive()) {
+                    pokemon1.attack(pokemon2, move2);
+                }
+            }
+            else {
+                pokemon1.attack(pokemon2, move2)
+                if (pokemon1.alive() && pokemon2.alive()) {
+                    pokemon2.attack(pokemon1, enemyMove);
+                }
+            }
+            turn ++;
+            enableButtons();
+        });
+
+        $("#move3").click(() => {
+            
+            disableButtons();
+            let i = Math.floor(Math.random() * 4);
+            let enemyMove = allMoves[pokemon2.moves[i]];
+            
+            if (pokemon1.speed < pokemon2.speed) {
+                pokemon2.attack(pokemon1, enemyMove);
+                if (pokemon1.alive() && pokemon2.alive()) {
+                    pokemon1.attack(pokemon2, move3);
+                }
+            }
+            else {
+                pokemon1.attack(pokemon2, move3)
+                if (pokemon1.alive() && pokemon2.alive()) {
+                    pokemon2.attack(pokemon1, enemyMove);
+                }
+            }
+            turn ++;
+            enableButtons();
+        });
+
+        $("#move4").click(() => {
+            
+            disableButtons();
+            let i = Math.floor(Math.random() * 4);
+            let enemyMove = allMoves[pokemon2.moves[i]];
+            
+            if (pokemon1.speed < pokemon2.speed) {
+                pokemon2.attack(pokemon1, enemyMove);
+                if (pokemon1.alive() && pokemon2.alive()) {
+                    pokemon1.attack(pokemon2, move4);
+                }
+            }
+            else {
+                pokemon1.attack(pokemon2, move4)
+                if (pokemon1.alive() && pokemon2.alive()) {
+                    pokemon2.attack(pokemon1, enemyMove);
+                }
+            }
+            turn ++;
+            enableButtons();
+        });
+
+        // ------------------------------------------------------------------------------------
+
+    }, 3000)
 
 });
--- scripts/test.js
+++ scripts/test.js
@@ -1,53 +1,10 @@
-// var battleStart = true;
-
-    // while (battleStart == true) {
-
-    //     var pokemon1 = [];
-    //     var pokemon2 = [];
-
-    //     // Move 1
-    //     $("#move1").click(() => {
-
-    //         pokemon1.attack(pokemon2, pokemon1.move1); // Maybe store all moves in arr and use random generator to choose specific move
-    //         pokemon2.attack(pokemon1, pokemon1.move1); // Randomize maybe?
-
-    //     });
-
-    //     // Move 2
-    //     $("#move2").click(() => {
-
-    //         pokemon1.attack(pokemon2, pokemon1.move2);
-
-    //     });
-
-    //     // Move 3
-    //     $("#move3").click(() => {
-
-    //         pokemon1.attack(pokemon2, pokemon1.move3);
-
-    //     });
-
-    //     // Move 4
-    //     $("#move4").click(() => {
-
-    //         pokemon1.attack(pokemon2, pokemon1.move4);
-
-    //     });
-
-    // }
-
-$(function() {
-
-    var pokedex = [];
-
-    $.get("https://pokeapi.co/api/v2/pokemon/", (result) => {
-        for (let i = 0; i < 5; i ++) {
-            let pokemon = new Pokemon(result[i].name);
-            let properties = $.get(result[i].url);
-            pokedex.push([pokemon, properties]);
-        }
-    });
-
-    console.log(pokedex);
-
-});
+let target = 100;
+let baseDamage = 25;
+function damageCalc (baseDamage) {
+    min = Math.ceil(85);
+    max = Math.floor(100);
+    return Math.floor(baseDamage * (Math.floor(Math.random() * (max - min + 1)) + min) / 100);
+}
+let damage = damageCalc(baseDamage);
+target -= damage;
+console.log(target);
