7f90a6a4883cb02569da3c9298eeeca6e5eba63d
Rigo-Hernandez <hernandez_rigoberto@outlook.com>
2018-12-21 10:25:26 -0600

    change

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+
 tippy('#skill1', {
     content: "The turn Wish is used, the user will make a wish. At the end of the next turn, the Pokémon in the user's current position will be healed by half its own maximum HP",
     delay: 100,
@@ -6,8 +7,28 @@ tippy('#skill1', {
     size: 'large',
     duration: 500,
     animation: 'scale',
-    
 })
+tippy('#skill2', {
+    content: "Protect prevents any attacks targeted at the user from striking, for the duration of the turn. It has priority +4 so will activate before most other moves. The move functions identically to Detect.",
+    delay: 100,
+    arrow: true,
+    arrowType: 'round',
+    size: 'large',
+    duration: 500,
+    animation: 'scale',
+})
+tippy('#skill3', {
+    content: "Toxic causes the target to be badly poisoned. Pokémon that are badly poisoned will lose an increasing amount of HP at the end of each turn.",
+    delay: 100,
+    arrow: true,
+    arrowType: 'round',
+    size: 'large',
+    duration: 500,
+    animation: 'scale',
+})
+
+
+
 // $(function() {
 //     $('#attackBtn').mouseover(function () { 
         
