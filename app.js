c962081e3f187cfbfcb0360c14c3d71bee94613c
RDCLder <riy2@case.edu>
2019-01-31 15:48:12 -0600

    changed to express layout -ray

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use(express.static("public"));
+app.use(require("./"));
+
+app.listen("3000", () => {
+	console.log("Listening on port 3000");
+});
